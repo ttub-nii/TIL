0d45403fa75b282c1c4e8f2dc6a620bb9ad98de5
ttub-nii <ostarangelo@naver.com>
2020-03-31 19:59:22 +0900

    velocity 사용하지 않고 scroll 방향 체크할 것
    UISwipeGestureRecognizer 또는 lastContentOffset 변수 선언하여 스크롤 문제 해결할 것

--- ScrollViewDelegate/ScrollViewDelegate/ViewController.swift
+++ ScrollViewDelegate/ScrollViewDelegate/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // @property (nonatomic) CGFloat lastContentOffset;
     @IBOutlet var logo: UILabel!
     @IBOutlet var topView: UIView!
     @IBOutlet var tableView: UITableView!
@@ -22,6 +23,13 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
     }
+    /*
+    @IBAction func swipeHandler(_ gestureRecognizer : UISwipeGestureRecognizer) {
+        if gestureRecognizer.state == .ended {
+            // Perform action.
+        }
+    }
+    */
 }
 
 extension ViewController : UITableViewDelegate {}
@@ -44,16 +52,19 @@ extension ViewController : UIScrollViewDelegate {
      navigationItem.searchController = searchController
      navigationItem.hidesSearchBarWhenScrolling = true
      */
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         
+        /*
         // 이동 제스처는 화면에서 손가락의 움직임을 감지하고 이 움직임을 콘텐츠에 적용하는 데 사용되며 PanGestureRecognizer 클래스로 구현합니다.
         let yVelocity = scrollView.panGestureRecognizer.velocity(in: scrollView).y
+        */
         
         // velocity 는 Pan Gesture의 속도입니다. 리턴값은 CGPoint값이며 초당 포인트(점)로 표시됩니다. 속도는 수평 및 수직 구성요소로 나뉩니다. 속력과 속도의 차이.. 아시죠?
-        if yVelocity > 0 {
+        if velocity.y < 0 {
             downHomeView()
             
-        } else if yVelocity < 0 {
+        } else if velocity.y > 0 {
             upHomeView()
         }
     }
@@ -63,14 +74,16 @@ extension ViewController : UIScrollViewDelegate {
         self.tableViewContraint.constant = 0
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
             
+            // guard self.tableView.contentOffset.y <= CGFloat(115) else { return }
+            print("contentOffset.y  \(self.tableView.contentOffset.y)")
+            
             // 사라지는데 효과를 주고 싶다면 opacity 값, 애니메이션 등을 사용해보세요.
-            //self.logo.alpha = 0
-            self.logo.textColor = .black
+            self.logo.alpha = 0
             self.topView.backgroundColor = .black
             
             // 상단바 위치 올리기
-            self.topView.transform = CGAffineTransform(translationX: 0, y: -115)
-            // self.topView.transform = CGAffineTransform(translationX: 0, y: -self.tableView.contentOffset.y)
+            // self.topView.transform = CGAffineTransform(translationX: 0, y: -115)
+            self.topView.transform = CGAffineTransform(translationX: 0, y: -self.tableView.contentOffset.y)
             self.view.layoutIfNeeded()
         })
     }
@@ -80,7 +93,7 @@ extension ViewController : UIScrollViewDelegate {
         self.tableViewContraint.constant = 213
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: {
             
-            // self.logo.alpha = 1
+            self.logo.alpha = 1
             self.logo.textColor = .white
             self.topView.backgroundColor = .systemPink
             
