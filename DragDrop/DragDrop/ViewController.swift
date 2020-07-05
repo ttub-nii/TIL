489629d79909fc5828ecc1f938b99e7425c38560
ttub-nii <ostarangelo@naver.com>
2020-07-05 23:19:23 +0900

    add: tableViewController

--- DragDrop/DragDrop/ViewController.swift
+++ DragDrop/DragDrop/ViewController.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
-
+import Foundation
+/*
 class ViewController: UIViewController {
-
+    
     @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.dragDelegate = self
     }
-
-
 }
 
+extension ViewController : UICollectionViewDelegate {
+    
+}
+
+extension ViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        
+        return cell
+    }
+}
+
+extension ViewController : UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        
+        return
+    }
+    
+    private func dragItem(indexPath: IndexPath) -> UIDragItem {
+        return
+    }
+}
+
+extension ViewController : UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+    }
+}
+*/
