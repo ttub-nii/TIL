58ae43a0fe455011092b1e92949bc84a11ddbe33
ttub-nii <ostarangelo@naver.com>
2020-07-24 21:25:15 +0900

    update: collectionView cell setting

---
+++ DragDrop/DragDrop/ImgCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImgCollectionViewCell.swift
+//  DragDrop
+//
+//  Created by 황수빈 on 2020/07/24.
+//  Copyright © 2020 황수빈. All rights reserved.
+//
+
+import UIKit
+
+class ImgCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+}
