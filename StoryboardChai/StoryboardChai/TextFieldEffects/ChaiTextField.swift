c5b2f8efc1ad754ada679fbabd5854480bcf315e
toby(황수빈) <ostarangelo@naver.com>
2021-07-15 16:42:41 +0900

    Create TextField Effects

---
+++ StoryboardChai/StoryboardChai/TextFieldEffects/ChaiTextField.swift
@@ -0,0 +1,110 @@
+//
+//  ChaiTextField.swift
+//  StoryboardChai
+//
+//  Created by toby.with on 2021/07/14.
+//
+
+import UIKit
+
+@IBDesignable open class ChaiTextField: TextFieldEffects {
+    @IBInspectable dynamic open var placeholderColor: UIColor = .black {
+        didSet {
+            updatePlaceholder()
+        }
+    }
+    
+    override open var placeholder: String? {
+        didSet {
+            updatePlaceholder()
+        }
+    }
+    
+    override open var bounds: CGRect {
+        didSet {
+            updatePlaceholder()
+        }
+    }
+    
+    private let placeholderInsets = CGPoint(x: 15, y: 15)
+    private let textFieldInsets = CGPoint(x: 15, y: 12)
+    
+    // MARK: - TextFieldEffects
+    override open func drawViewsForRect(_ rect: CGRect) {
+        let frame = CGRect(origin: CGPoint.zero, size: CGSize(width: rect.size.width, height: rect.size.height))
+        
+        placeholderLabel.frame = frame.insetBy(dx: placeholderInsets.x, dy: placeholderInsets.y)
+        
+        updatePlaceholder()
+        addSubview(placeholderLabel)
+    }
+    
+    override open func animateViewsForTextEntry() {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1.0, options: .beginFromCurrentState, animations: {
+            self.placeholderLabel.frame.origin = CGPoint(x: self.placeholderInsets.x, y: 15)
+            self.placeholderLabel.font = self.placeholderLabel.font.withSize(12)
+            // 추가
+//            self.borderLayer.borderColor = self.textColor?.cgColor
+//            self.borderLayer.shadowOffset = CGSize.zero
+//            self.borderLayer.borderWidth = self.borderThickness
+//            self.borderLayer.shadowColor = self.textColor?.cgColor
+//            self.borderLayer.shadowOpacity = 0.5
+//            self.borderLayer.shadowRadius = 1
+        }, completion: { _ in
+            self.animationCompletionHandler?(.textEntry)
+        })
+    }
+    
+    override open func animateViewsForTextDisplay() {
+        if text!.isEmpty {
+            UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1.0, options: .beginFromCurrentState, animations: {
+                self.layoutPlaceholderInTextRect()
+                self.placeholderLabel.alpha = 1
+                self.placeholderLabel.font = self.placeholderLabel.font.withSize(17)
+            }, completion: { _ in
+                self.animationCompletionHandler?(.textDisplay)
+            })
+        }
+    }
+    
+    // MARK: - Private
+    private func updatePlaceholder() {
+        placeholderLabel.text = placeholder
+        placeholderLabel.textColor = placeholderColor
+        placeholderLabel.sizeToFit()
+        layoutPlaceholderInTextRect()
+        
+        if isFirstResponder || text!.isNotEmpty {
+            animateViewsForTextEntry()
+        }
+    }
+    
+    private func layoutPlaceholderInTextRect() {
+        if text!.isNotEmpty {
+            return
+        }
+        
+        let textRect = self.textRect(forBounds: bounds)
+        var originX = textRect.origin.x
+        switch self.textAlignment {
+        case .center:
+            originX += textRect.size.width/2 - placeholderLabel.bounds.width/2
+        case .right:
+            originX += textRect.size.width - placeholderLabel.bounds.width
+        default:
+            break
+        }
+        placeholderLabel.frame = CGRect(x: originX, y: textRect.size.height/2 - 10,
+            width: placeholderLabel.frame.size.width, height: placeholderLabel.frame.size.height)
+    }
+    
+    // MARK: - Overrides
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.offsetBy(dx: textFieldInsets.x, dy: textFieldInsets.y)
+    }
+    
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.offsetBy(dx: textFieldInsets.x, dy: textFieldInsets.y)
+    }
+
+}
---
+++ StoryboardChai/StoryboardChai/TextFieldEffects/TextFieldEffects.swift
@@ -0,0 +1,125 @@
+//
+//  TextFieldEffects.swift
+//  StoryboardChai
+//
+//  Created by toby.with on 2021/07/14.
+//
+
+import UIKit
+
+extension String {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}
+
+/**
+텍스트가 입력되거나 사라지는 시점에 애니메이션 효과를 주기 위한 UITextField 템플릿
+*/
+open class TextFieldEffects : UITextField {
+    /**
+     애니메이션 타입을 지정
+     
+     - TextEntry: 텍스트 필드가 활성화되었을 때
+     - TextDisplay: 텍스트 필드가 비활성화되었을 때
+     */
+    public enum AnimationType: Int {
+        case textEntry
+        case textDisplay
+    }
+    
+    /** 애니메이션이 끝났을 때 실행되는 클로저 */
+    public typealias AnimationCompletionHandler = (_ type: AnimationType)->()
+    
+    public let placeholderLabel = UILabel()
+    
+    /**
+    Creates all the animations that are used to leave the textfield in the "entering text" state.
+    */
+    open func animateViewsForTextEntry() {
+        fatalError("\(#function) must be overridden")
+    }
+    
+    /**
+    Creates all the animations that are used to leave the textfield in the "display input text" state.
+    */
+    open func animateViewsForTextDisplay() {
+        fatalError("\(#function) must be overridden")
+    }
+    
+    /**
+     The animation completion handler is the best place to be notified when the text field animation has ended.
+     */
+    open var animationCompletionHandler: AnimationCompletionHandler?
+    
+    /**
+    Draws the receiver’s image within the passed-in rectangle.
+    
+    - parameter rect:    The portion of the view’s bounds that needs to be updated.
+    */
+    open func drawViewsForRect(_ rect: CGRect) {
+        fatalError("\(#function) must be overridden")
+    }
+    
+    open func updateViewsForBoundsChange(_ bounds: CGRect) {
+        fatalError("\(#function) must be overridden")
+    }
+    
+    // MARK: - Overrides
+    
+    override open func draw(_ rect: CGRect) {
+        // FIXME: Short-circuit if the view is currently selected. iOS 11 introduced
+        // a setNeedsDisplay when you focus on a textfield, calling this method again
+        // and messing up some of the effects due to the logic contained inside these
+        // methods.
+        // This is just a "quick fix", something better needs to come along.
+        guard isFirstResponder == false else { return }
+        drawViewsForRect(rect)
+    }
+    
+    override open func drawPlaceholder(in rect: CGRect) {
+        // Don't draw any placeholders
+    }
+    
+    override open var text: String? {
+        didSet {
+            if let text = text, !text.isEmpty || isFirstResponder {
+                animateViewsForTextEntry()
+            } else {
+                animateViewsForTextDisplay()
+            }
+        }
+    }
+    
+    // MARK: - UITextField Observing
+    
+    override open func willMove(toSuperview newSuperview: UIView!) {
+        if newSuperview != nil {
+            NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidEndEditing), name: UITextField.textDidEndEditingNotification, object: self)
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidBeginEditing), name: UITextField.textDidBeginEditingNotification, object: self)
+        } else {
+            NotificationCenter.default.removeObserver(self)
+        }
+    }
+    
+    /**
+    The textfield has started an editing session.
+    */
+    @objc open func textFieldDidBeginEditing() {
+        animateViewsForTextEntry()
+    }
+    
+    /**
+    The textfield has ended an editing session.
+    */
+    @objc open func textFieldDidEndEditing() {
+        animateViewsForTextDisplay()
+    }
+    
+    // MARK: - Interface Builder
+    
+    override open func prepareForInterfaceBuilder() {
+        drawViewsForRect(frame)
+    }
+}
