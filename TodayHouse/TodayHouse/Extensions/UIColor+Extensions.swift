35c46d795ed20ab73c0e383b5b3d641efea9c1f3
Subin Hwang <ostarangelo@naver.com>
2021-07-29 21:37:18 +0900

    Create UIColor+Extensions.swift

---
+++ TodayHouse/TodayHouse/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extensions.swift
+//  TodayHouse
+//
+//  Created by toby.with on 2021/07/29.
+//
+
+import UIKit
+
+extension UIColor {
+  @nonobjc class var main: UIColor {
+    return UIColor(red: 52.0 / 255.0, green: 191.0 / 255.0, blue: 233.0 / 255.0, alpha: 1.0)
+  }
+}
