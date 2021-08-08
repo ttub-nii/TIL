19b58de69cb4180f2f2dac46cc2be92f3c1ac41c
Subin Hwang <ostarangelo@naver.com>
2021-08-09 02:59:47 +0900

    Delete needless initializer

--- iACC/iACC/ListViewController.swift
+++ iACC/iACC/ListViewController.swift
@@ -185,18 +185,6 @@ struct ItemViewModel {
     let title: String
     let subtitle: String
     let select: () -> Void
-    
-    init(_ item: Any, longDateStyle: Bool, selection: @escaping () -> Void) {
-        if let friend = item as? Friend {
-            self.init(friend: friend, selection: selection)
-        } else if let card = item as? Card {
-            self.init(card: card, selection: selection)
-        } else if let transfer = item as? Transfer {
-            self.init(transfer: transfer, longDateStyle: longDateStyle, selection: selection)
-        } else {
-            fatalError("unknown item: \(item)")
-        }
-    }
 }
 
 extension ItemViewModel {
