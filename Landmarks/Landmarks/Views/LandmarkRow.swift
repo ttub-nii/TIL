5c0bb970a899076f13e2e17549ac1e8fb861b871
Subin Hwang <ostarangelo@naver.com>
2022-01-07 23:08:41 +0900

    [SwiftUI Tutorials - Building Lists and Navigation] Create LandmarkRow

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by toby.with on 2022/01/07.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.image
+            .resizable()
+            .frame(width: 50, height: 50)
+        Text(landmark.name)
+        
+        Spacer()
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
