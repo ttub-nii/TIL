67159e1748e75690767ed05a6accaf27f2f67554
Subin Hwang <ostarangelo@naver.com>
2022-01-07 23:40:05 +0900

    [SwiftUI Tutorials - Building Lists and Navigation] Generate previews dynamically

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -37,6 +37,12 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+//        LandmarkList()
+//            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
