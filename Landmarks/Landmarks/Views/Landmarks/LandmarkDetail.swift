8c38a7e1f3d6a08aa481634f595fa2f83e1640b5
Subin Hwang <ostarangelo@naver.com>
2022-01-08 23:58:32 +0900

    [SwiftUI Tutorials - Handling User Input] Bind 'isSet' property to 'isFavorite' property

--- Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -21,8 +26,11 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
