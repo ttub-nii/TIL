e003deadeb7c8d15b6559fa278715bd7e7ddb848
Subin Hwang <ostarangelo@naver.com>
2022-01-08 23:49:29 +0900

    [SwiftUI Tutorials - Handling User Input] Create a Favorite Button

---
+++ Landmarks/Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by toby.with on 2022/01/08.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
