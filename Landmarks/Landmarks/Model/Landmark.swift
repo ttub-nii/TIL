c51d6169f33ed609b0c5bb84c6fcbbd62e2a072d
Subin Hwang <ostarangelo@naver.com>
2022-01-09 17:58:24 +0900

    [SwiftUI Tutorials - Drawing Paths and Shapes] Draw Badge

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -26,7 +26,8 @@ struct Landmark: Hashable, Codable, Identifiable {
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
-            longitude: coordinates.longitude)
+            longitude: coordinates.longitude
+        )
     }
     
     
