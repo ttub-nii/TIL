2b06084e00f10c193a67c62d452e166ef8445af4
Subin Hwang <ostarangelo@naver.com>
2022-01-12 13:32:41 +0900

    [SwiftUI Tutorials - Composing Complex Interfaces] Create a Category List

--- Landmarks/Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
         NavigationView {
-            Text("Hello, World!")
-                .navigationTitle("Featured")
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         }
     }
 }
@@ -19,5 +25,6 @@ struct CategoryHome: View {
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            .environmentObject(ModelData())
     }
 }
