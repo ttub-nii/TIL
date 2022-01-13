7b94b8218ec80882d0fc0dde46599257118659d7
Subin Hwang <ostarangelo@naver.com>
2022-01-13 18:20:06 +0900

    [SwiftUI Tutorials - Working with UI Controls] Update ProfileHost to display the new summary view

--- Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -11,7 +11,10 @@ struct ProfileHost: View {
     @State private var draftProfile = Profile.default
     
     var body: some View {
-        Text("Profile for: \(draftProfile.username)")
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
     }
 }
 
