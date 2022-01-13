b3cdcabcbc3e7aaa56da370ff4001d5c2e70d776
Subin Hwang <ostarangelo@naver.com>
2022-01-13 18:30:07 +0900

    [SwiftUI Tutorials - Working with UI Controls] Update ProfileSummary
    - Add several badges with varying hues and reasons for earning the badge

--- Landmarks/Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileSummary.swift
@@ -20,6 +20,25 @@ struct ProfileSummary: View {
                 Text("Notifications: \(profile.prefersNotifications ? "On": "Off")")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding()
+                    }
+                }
             }
         }
     }
