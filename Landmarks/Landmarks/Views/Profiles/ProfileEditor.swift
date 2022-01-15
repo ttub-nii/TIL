60ed6cfb5c88886d7c6702d4587b786393831170
Subin Hwang <ostarangelo@naver.com>
2022-01-15 19:17:30 +0900

    [SwiftUI Tutorials - Working with UI Controls] Create ProfileEditor
    - Update the conditional content in ProfileHost to include the profile editor and pass along the profile binding
    - Display the edit profile view when tap Edit

---
+++ Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by toby.with on 2022/01/14.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
