701ca31da148b4734c6b4c7809ec802caf374560
Subin Hwang <ostarangelo@naver.com>
2022-01-16 21:32:29 +0900

    [SwiftUI Tutorials - Working with UI Controls] Add Segmented Picker

--- Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -23,6 +23,18 @@ struct ProfileEditor: View {
                 Text("Enable Notifications")
                     .bold()
             }
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue)
+                            .tag(season)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
         }
     }
 }
