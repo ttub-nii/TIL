2764a44e8aaf502684c49b6d313e5f6d1665e01f
Subin Hwang <ostarangelo@naver.com>
2022-01-16 21:34:03 +0900

    [SwiftUI Tutorials - Working with UI Controls] Add DatePicker

--- Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct ProfileEditor: View {
     @Binding var profile: Profile
     
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
     var body: some View {
         List {
             HStack {
@@ -34,6 +40,11 @@ struct ProfileEditor: View {
                     }
                 }
                 .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date")
+                    .bold()
             }
         }
     }
