56047d612d164a66ecf64952f46d85cc5334dfe7
Subin Hwang <ostarangelo@naver.com>
2022-01-18 22:15:43 +0900

    [SwiftUI Tutorials - Interfacing with UIKit] Create PageView
    - Create custom view to present UIViewControllerRepresentable view
    - Update the preview provider to pass the required array of views

---
+++ Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,25 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by toby.with on 2022/01/18.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(
+            pages: ModelData().features.map { FeatureCard(landmark: $0) }
+        )
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
