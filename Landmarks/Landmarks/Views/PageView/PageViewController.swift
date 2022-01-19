1ec4b4b80a45b05b1072685afad57925b2ac807e
Subin Hwang <ostarangelo@naver.com>
2022-01-19 23:26:47 +0900

    [SwiftUI Tutorials - Interfacing with UIKit] Create the View Controller’s Data Source
    - Add the coordinator as the data source of the UIPageViewController

--- Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -20,9 +20,9 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
-            navigationOrientation: .horizontal,
-            options: .none
+            navigationOrientation: .horizontal
         )
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
@@ -35,13 +35,37 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
         )
     }
     
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
         var parent: PageViewController
         var controllers = [UIViewController]()
         
         init(_ pageViewController: PageViewController) {
             parent = pageViewController
             controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index == 0 {
+                return controllers.last
+            }
+            
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            
+            return controllers[index + 1]
         }
     }
 }
