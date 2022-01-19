0a337f43d92a523194b6e96d56c795deb2f5f7ce
Subin Hwang <ostarangelo@naver.com>
2022-01-19 23:21:29 +0900

    [SwiftUI Tutorials - Interfacing with UIKit] Declare a nested Coordinator class

--- Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     // SwiftUI calls this method a single time when it’s ready to display the view,
     // and then manages the view controller’s life cycle.
     func makeUIViewController(context: Context) -> UIPageViewController {
@@ -25,9 +29,19 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
 
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])],
+            [context.coordinator.controllers[0]],
             direction: .forward,
             animated: true
         )
+    }
+    
+    class Coordinator: NSObject {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
     }
 }
