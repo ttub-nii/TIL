c8c7f44e27264c5665a13772b0af8122a3480129
Subin Hwang <ostarangelo@naver.com>
2022-01-20 23:56:32 +0900

    [SwiftUI Tutorials - Creating a watchOS App] Add LandmarkDetail custom view

---
+++ Landmarks/WatchLandmarks Extension/LandmarkDetail.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks Extension
+//
+//  Created by toby.with on 2022/01/20.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        let modelData = ModelData()
+        return LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
+    }
+}
