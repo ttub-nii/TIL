f15b809587267c7700023d3f7dc104c32044c6fd
Subin Hwang <ostarangelo@naver.com>
2022-01-20 23:34:45 +0900

    [SwiftUI Tutorials - Creating a watchOS App] Add Watch App Target

---
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by toby.with on 2022/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
