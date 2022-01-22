e0f3a1e9c9a1bf2a6b67559a7ffae4e659a1e929
Subin Hwang <ostarangelo@naver.com>
2022-01-22 23:40:34 +0900

    [SwiftUI Tutorials - Creating a watchOS App] Update NotificationView to Display information

--- Landmarks/WatchLandmarks Extension/NotificationView.swift
+++ Landmarks/WatchLandmarks Extension/NotificationView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if landmark != nil {
+                CircleImage(image: landmark?.image.resizable() ?? Image("circle"))
+                    .scaledToFit()
+            }
+            
+            Text(title ?? "Unknown Landmark")
+                .font(.headline)
+            
+            Divider()
+            
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+                .font(.caption)
+        }
+        .lineLimit(0)
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        Group {
+            NotificationView()
+            NotificationView(title: "Turtle Rock",
+                             message: "You are within 5 miles of Turtle Rock.",
+                             landmark: ModelData().landmarks[0])
+        }
     }
 }
