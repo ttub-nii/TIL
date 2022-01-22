03120fc05188dfabebaff8c7644a9ed7314e3faf
Subin Hwang <ostarangelo@naver.com>
2022-01-22 23:23:23 +0900

    [SwiftUI Tutorials - Creating a watchOS App] Create the Detail View

--- Landmarks/WatchLandmarks Extension/LandmarkDetail.swift
+++ Landmarks/WatchLandmarks Extension/LandmarkDetail.swift
@@ -16,14 +16,51 @@ struct LandmarkDetail: View {
     }
 
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+                
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                
+                Divider()
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+                
+                Divider()
+                
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         let modelData = ModelData()
-        return LandmarkDetail(landmark: modelData.landmarks[0])
-            .environmentObject(modelData)
+        return Group {
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 44mm")
+            
+            LandmarkDetail(landmark: modelData.landmarks[1])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 40mm")
+        }
     }
 }
