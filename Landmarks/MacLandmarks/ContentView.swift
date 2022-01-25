a2b06ba1d1e1c022b123db04d73ea81a5fdf8db1
Subin Hwang <ostarangelo@naver.com>
2022-01-25 20:12:21 +0900

    [SwiftUI Tutorials - Creating a macOS App] Add a macOS Target to the Project

---
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by toby.with on 2022/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
