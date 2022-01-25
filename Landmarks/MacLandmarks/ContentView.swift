6bae9859f162b9bf8bf8a2795fe7b3bee8ff40e0
Subin Hwang <ostarangelo@naver.com>
2022-01-25 20:15:13 +0900

    [SwiftUI Tutorials - Creating a macOS App] Add LandmarkList In ContentView

--- Landmarks/MacLandmarks/ContentView.swift
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
