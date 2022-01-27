faa724d3aa953a91e6b9716bcfe3087b63df0e66
Subin Hwang <ostarangelo@naver.com>
2022-01-28 00:28:08 +0900

    [SwiftUI Tutorials - Creating a macOS App] Add LandmarkSettings

---
+++ Landmarks/MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by toby.with on 2022/01/28.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    @AppStorage("MapView.zoom")
+    private var zoom: MapView.Zoom = .medium
+    
+    var body: some View {
+        Form {
+            Picker("Map Zoom:", selection: $zoom) {
+                ForEach(MapView.Zoom.allCases) { level in
+                    Text(level.rawValue)
+                }
+            }
+            .pickerStyle(InlinePickerStyle())
+        }
+        .frame(width: 300)
+        .navigationTitle("Landmark Settings")
+        .padding(80)
+    }
+}
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
