e46a72dd5e27bd80e178380e5cdfa412decfcb95
Subin Hwang <ostarangelo@naver.com>
2022-01-28 23:32:01 +0900

    [CardAnimation] 프로젝트 생성

---
+++ CardAnimation/CardAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardAnimation
+//
+//  Created by toby.with on 2022/01/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
